bef6487c7270a7a53f71ae69c3ac365c858ae95c
AminKei <moakeimasi@gmail.com>
2025-06-25 14:06:04 +0330

    fix bugs

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -1,7 +1,7 @@
 import { User } from "../types/user";
 
 export async function fetchUser(): Promise<User> {
-  const response = await fetch(process.env.NEXT_PUBLIC_API as string);
+  const response = await fetch("https://randomuser.me/api/?results=1&nat=us");
   if (!response.ok) {
     throw new Error("Failed to fetch user");
   }
